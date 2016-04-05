fcaa4ee9d0a41850cc8fa127cd96fb2683a6bdfe
qingfengykl <qingfengykl@163.com>
2016-04-05 20:12:25 +0800

    给发现栏目添加viewPager和内容fragment

--- app/src/main/java/com/example/kelin/zhufengfm/fragment/DiscoveryFragment.java
+++ app/src/main/java/com/example/kelin/zhufengfm/fragment/DiscoveryFragment.java
@@ -8,14 +8,18 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.kelin.zhufengfm.R;
+import com.example.kelin.zhufengfm.adapter.DiscoveryAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DiscoveryFragment extends Fragment implements TabLayout.OnTabSelectedListener {
 
+
+    private ViewPager mPager;
 
     public DiscoveryFragment() {
         // Required empty public constructor
@@ -29,41 +33,47 @@ public class DiscoveryFragment extends Fragment implements TabLayout.OnTabSelect
         View ret = inflater.inflate(R.layout.discovery_fragment, container, false);
 //        1.获取tabBar
 
-        TabLayout tabBar = (TabLayout) ret.findViewById(R.id.discovery_tab_bar);
+        final TabLayout tabBar = (TabLayout) ret.findViewById(R.id.discovery_tab_bar);
         //必须设置OnTabSelectListener
         tabBar.setOnTabSelectedListener(this);
-
-        TabLayout.Tab tab = tabBar.newTab();
-        tab.setText("推荐");
-        tabBar.addTab(tab);
-
-         tab = tabBar.newTab();
-        tab.setText("分类");
-        tabBar.addTab(tab);
-
-         tab = tabBar.newTab();
-        tab.setText("广播");
-        tabBar.addTab(tab);
-
-         tab = tabBar.newTab();
-        tab.setText("榜单");
-        tabBar.addTab(tab);
-
-         tab = tabBar.newTab();
-        tab.setText("主播");
-        tabBar.addTab(tab);
+//
+//        TabLayout.Tab tab = tabBar.newTab();
+//        tab.setText("推荐");
+//        tabBar.addTab(tab);
+//
+//         tab = tabBar.newTab();
+//        tab.setText("分类");
+//        tabBar.addTab(tab);
+//
+//         tab = tabBar.newTab();
+//        tab.setText("广播");
+//        tabBar.addTab(tab);
+//
+//         tab = tabBar.newTab();
+//        tab.setText("榜单");
+//        tabBar.addTab(tab);
+//
+//         tab = tabBar.newTab();
+//        tab.setText("主播");
+//        tabBar.addTab(tab);
 
         // TODO: TabLayout 与 ViewPager联动
 
-        ViewPager pager = null;
+        mPager = (ViewPager) ret.findViewById(R.id.discovery_fragment_viewpager);
+        DiscoveryAdapter adapter = new DiscoveryAdapter(getActivity().getSupportFragmentManager());
+        mPager.setAdapter(adapter);
+//        mPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabBar));
 
-
+        tabBar.setupWithViewPager(mPager);
         return ret;
     }
 
     @Override
     public void onTabSelected(TabLayout.Tab tab) {
-
+        int position = tab.getPosition();
+        if (mPager != null) {
+            mPager.setCurrentItem(position);
+        }
     }
 
     @Override
